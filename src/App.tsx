9b2416353b567214bb1b055f80c91b252444c6d0
Lucas Ch <lucaschiurazzi@stacktrace.com.ar>
2020-07-30 22:47:51 -0300

    text input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Button from "./components/Button/Button";
+import TextInput from "./TextInput/TextInput";
 
 function App() {
   return (
     <div className="App container mx-auto">
       <h1 className="text-5x1">Spider</h1>
+        <div className="py-10">
+            <TextInput label="Enter a word or phrase" type="password"
+                        />
+            <Button onClick={() => alert('Hello world!')} text='OK'/>
+        </div>
     </div>
   );
 }
